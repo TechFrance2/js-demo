477408260d2161082acc8bb4ef392b5d3ba3e171
Francelli Nakamura <franci.nakamura100@gmail.com>
2021-11-11 08:28:32 -0800

    hide boots

--- home.js
+++ home.js
@@ -1,14 +1,3 @@
-const values = [24, 89, 12, 16];
-
-const cost = values.filter(function (item) {
-  return item > 16;
-});
-
-const priceSaleItems = cost.find(function (item) {
-  return item < 89;
-});
-
-values.forEach(function (item) {
-  console.log(item);
-});
-showMessage("$" + priceSaleItems);
+const containers = document.getElementsByClassName("container");
+containers[2].classList.add("d-none");
+console.log(containers);
